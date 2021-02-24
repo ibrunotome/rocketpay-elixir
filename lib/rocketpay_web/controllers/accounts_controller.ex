e44cc8807a67a89e88a2e7f9ff4cb95f7c1befdf
Bruno Tomé <ibrunotome@gmail.com>
2021-02-24 20:23:56 -0300

    feat: criando contas e saldo

---
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,13 @@
+defmodule RocketpayWeb.AccountsController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Account
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+end
